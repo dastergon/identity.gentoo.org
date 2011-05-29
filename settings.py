126a92c4708577aa19680f5dedfd05b141b411b5
Theo Chatzimichos <tampakrap@gentoo.org>
2011-05-30 01:33:37 +0300

    New way to handle settings (based on the transifex way) settings/ dir contains .conf files so that we can distribute them even in /etc, and split them more if needed Sample files have .sample suffix now

--- settings.py
+++ settings.py
@@ -1,3 +1,17 @@
-import okupy.settings.base
+import os.path
+import glob
 
-from okupy.settings import base
+try:
+	PROJECT_ROOT
+except NameError:
+	PROJECT_ROOT = os.path.dirname(__file__)
+
+print PROJECT_ROOT
+
+conf_files_path = os.path.join(PROJECT_ROOT, 'settings', '*.conf')
+conffiles = glob.glob(conf_files_path)
+conffiles.sort()
+
+for f in conffiles:
+	print f
+	execfile(os.path.abspath(f))
