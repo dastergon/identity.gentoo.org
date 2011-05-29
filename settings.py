b4f2e1a514fa325f6bebdd8de0971b4d95383d29
Theo Chatzimichos <tampakrap@gentoo.org>
2011-05-30 02:45:01 +0300

    Remove prints from settings.py

--- settings.py
+++ settings.py
@@ -6,12 +6,9 @@ try:
 except NameError:
 	PROJECT_ROOT = os.path.dirname(__file__)
 
-print PROJECT_ROOT
-
 conf_files_path = os.path.join(PROJECT_ROOT, 'settings', '*.conf')
 conffiles = glob.glob(conf_files_path)
 conffiles.sort()
 
 for f in conffiles:
-	print f
 	execfile(os.path.abspath(f))
