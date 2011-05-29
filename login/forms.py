aa53a753b78bb54a12aca9ccc51c0de3ab589433
Theo Chatzimichos <tampakrap@gentoo.org>
2011-05-30 00:35:43 +0300

    Add login app

---
+++ login/forms.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+from django import forms
+
+class LoginForm(forms.Form):
+		username = forms.CharField(max_length = 30, label = 'Username:')
+		password = forms.CharField(max_length = 30, widget = forms.PasswordInput(), label = 'Password:')
+		remember = forms.BooleanField(required = False, label = 'Remember Me')
