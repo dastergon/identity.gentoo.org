ed8a5b8f34614997a13cdcda03dc4988c1cf4090
Theo Chatzimichos <tampakrap@gentoo.org>
2011-05-30 04:32:44 +0300

    Remove comments, properly enable admin panel

--- urls.py
+++ urls.py
@@ -5,15 +5,6 @@ from okupy.login.views import *
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'okupy.views.home', name='home'),
-    # url(r'^okupy/', include('okupy.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
-
 	url(r'^$', mylogin)
+    url(r'^admin/', include(admin.site.urls)),
 )
