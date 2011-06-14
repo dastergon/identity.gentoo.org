2055fc1eda896103931eaba5fb01238506aaac1a
Theo Chatzimichos <tampakrap@gentoo.org>
2011-06-14 06:31:08 +0300

    Add signup in urls

--- urls.py
+++ urls.py
@@ -2,11 +2,13 @@ from django.conf.urls.defaults import patterns, include, url
 from django.contrib import admin
 from okupy.login.views import *
 from okupy.user.views import *
+from okupy.signup.views import *
 
 admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^login/$', mylogin),
     url(r'^$', user),
+    url(r'^signup/', signup),
     url(r'^admin/', include(admin.site.urls)),
 )
