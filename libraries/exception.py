b992f8ca9d7e4269592dd4fe8129a7afe92634a7
Theo Chatzimichos <tampakrap@gentoo.org>
2011-06-21 19:28:30 +0300

    Add a log_extra_data function, which returns a dictionary needed by the logging formatter Add comments in the lib

--- libraries/exception.py
+++ libraries/exception.py
@@ -1,5 +1,35 @@
+from django.contrib.sites.models import Site
+
+
 class OkupyException(Exception):
-	def __init__(self, value):
-		self.value = value
-	def __str__(self):
-		return repr(self.value)
+    '''
+    Custon exception class
+    '''
+    def __init__(self, value):
+        self.value = value
+    def __str__(self):
+        return repr(self.value)
+
+def log_extra_data(request = None, form = None):
+    '''
+    Extra data needed by the custom formatter
+    All values default to None
+    '''
+    log_extra_data = {
+        'site_name': Site.objects.get_current().name or 'None',
+        'clientip':request.META.get('REMOTE_ADDR','None'),
+    }
+    if form:
+        log_extra_data['username'] = form.data.get('username','None')
+    else:
+        if request.user.is_authenticated():
+            '''
+            Handle logged in users
+            '''
+            log_extra_data['username'] = request.user.name
+        else:
+            '''
+            Handle anonymous users
+            '''
+            log_extra_data['username'] = 'Anonymous'
+    return log_extra_data
