e2af898b7bc593b7ea474a3c34b4e311dca29629
Theo Chatzimichos <tampakrap@gentoo.org>
2011-06-22 23:17:22 +0300

    Create the accounts view, which will list the user profile data and let him edit that data

---
+++ accounts/views.py
@@ -0,0 +1,11 @@
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+
+@login_required
+def account(request):
+    return render_to_response('account/account.html', {}, context_instance = RequestContext(request))
+
+@login_required
+def account_edit(request):
+    return render_to_response('account/edit.html', {}, context_instance = RequestContext(request))
--- urls.py
+++ urls.py
@@ -1,14 +1,14 @@
 from django.conf.urls.defaults import patterns, include, url
 from django.contrib import admin
 from okupy.login.views import *
-from okupy.user.views import *
 from okupy.signup.views import *
 
 admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^login/$', mylogin),
-    url(r'^$', user),
+    url(r'^$', include('index.urls')),
     url(r'^signup/', signup),
+    url(r'^account/', include('accounts.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
