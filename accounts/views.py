0e22badf2acb4a1c60f45dbd3df505dfe6a6cde2
Theo Chatzimichos <tampakrap@gentoo.org>
2011-06-23 03:29:16 +0300

    Fix accounts view to work with username urls, add empty template

--- accounts/views.py
+++ accounts/views.py
@@ -3,9 +3,9 @@ from django.shortcuts import render_to_response
 from django.template import RequestContext
 
 @login_required
-def account(request):
+def account(request, username):
     return render_to_response('account/account.html', {}, context_instance = RequestContext(request))
 
 @login_required
-def account_edit(request):
+def account_edit(request, username):
     return render_to_response('account/edit.html', {}, context_instance = RequestContext(request))
