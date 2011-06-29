32a734ef3e7c55c272ed6be160c83a8e36d3e84d
Theo Chatzimichos <tampakrap@gentoo.org>
2011-06-29 19:39:26 +0300

    Change ldap backend to support mail logins instead of username logins Improve the looging a bit

--- accounts/backends.py
+++ accounts/backends.py
@@ -12,7 +12,7 @@ class LDAPBackend(object):
     '''
     LDAP authentication backend
     '''
-    def authenticate(self, username = None, password = None):
+    def authenticate(self, mail = None, password = None):
         '''
         Try to authenticate the user. If there isn't such user
         in the Django DB, and assuming the credentials are correct,
@@ -21,7 +21,7 @@ class LDAPBackend(object):
         '''
         if not password:
             return None
-        return self.get_or_create_user(username, password)
+        return self.get_or_create_user(mail, password)
 
 
     def get_user(self, user_id):
@@ -33,7 +33,7 @@ class LDAPBackend(object):
         except User.DoesNotExist:
             return None
 
-    def get_or_create_user(self, username, password, other_username = None, other_password = None):
+    def get_or_create_user(self, mail, password, other_username = None, other_password = None):
         '''
         Retrieves a user from the Django DB. If the user is not
         found in the DB, then it tries to retrieve it from the
@@ -43,12 +43,12 @@ class LDAPBackend(object):
         then moved to the Django DB.
         '''
         try:
-            user = User.objects.get(username = username)
-        except User.DoesNotExist:
+            user = User.objects.get(gentooprofile__all_mails__contains = mail)
+        except (User.DoesNotExist, ValueError):
             '''
             Perform a search to find the user in the LDAP server.
             '''
-            results = ldap_user_search(username)
+            results = ldap_user_search(mail, 'mail')
             if not results:
                 return None
             '''
@@ -60,22 +60,24 @@ class LDAPBackend(object):
             to retrieve other users' data, other is defined,
             then it doesn't need to bind.
             '''
+            username = results[0][1]['uid'][0]
             original_username = username
-            if other:
+            if other_username:
                 username = other_username
                 password = other_password
+            l_user = None
             for ldap_base_dn in settings.LDAP_BASE_DN:
-                l_user = ldap_bind(username, password,
+                try:
+                    l_user = ldap_bind(username, password,
                                 settings.LDAP_BASE_ATTR,
                                 ldap_base_dn)
-                '''
-                Again, we need to search in multiple OU's for
-                the user's existence.
-                '''
-                try:
+                    '''
+                    Again, we need to search in multiple OU's for
+                    the user's existence.
+                    '''
                     if l_user:
                         break
-                except AttributeError:
+                except:
                     pass
             if not l_user:
                 return None
@@ -83,8 +85,7 @@ class LDAPBackend(object):
             Perform another search as the current user, to get
             all his data
             '''
-            results = l_user.ldap_user_search(original_username, 'uid', None, False)
-            l_user.unbind()
+            results = ldap_user_search(original_username, anon = False, l = l_user)
             '''
             In case everything went fine so far, it means there is
             a valid user available. Last step is to migrate the user's
@@ -92,21 +93,24 @@ class LDAPBackend(object):
             '''
             user = User()
             for field, attr in settings.LDAP_USER_ATTR_MAP.iteritems():
-                setattr(user, field, results[0][1][attr][0])
+                try:
+                    setattr(user, field, results[0][1][attr][0])
+                except Exception as error:
+                    logger.error(error, extra = log_extra_data())
+                    raise OkupyException('LDAP User Attribute Map is invalid')
             user.set_unusable_password()
-            try:
-                user.save()
-            except Exception as error:
-                logger.error(error, extra = log_extra_data())
-                raise OkupyException('Could not save to DB')
             '''
             Additional data that should be put in the user's profile
             '''
             try:
                 if settings.LDAP_PROFILE_ATTR_MAP:
-                    user_profile = settings.AUTH_PROFILE_MODULE
+                    user_profile = eval(settings.AUTH_PROFILE_MODULE.split('accounts.')[1] + '()')
                     for field, attr in settings.LDAP_PROFILE_ATTR_MAP.iteritems():
-                        setattr(user_profile, field, '::'.join(results[0][1][attr]))
+                        try:
+                            setattr(user_profile, field, '::'.join(results[0][1][attr]))
+                        except Exception as error:
+                            logger.error(error, extra = log_extra_data())
+                            raise OkupyException('LDAP Profile Attribute Map is invalid')
                     '''
                     Check if the user is member of the groups under
                     settings.LDAP_ACL_GROUPS. In order to do this, the
@@ -115,16 +119,23 @@ class LDAPBackend(object):
                     For correct results, it sets the according is_field
                     of the UserProfile to True.
                     '''
-                    for attr, field in settings.LDAP_ACL_GROUPS.iteritems():
-                        if attr in results[0][1][settings.LDAP_ACL_ATTR]:
-                            setattr(user_profile, field, True)
-
-                    try:
-                        user_profile.save()
-                    except Exception as error:
-                        logger.error(error, extra = log_extra_data(request))
-                        raise OkupyException('Could not save to DB')
+                    for field, attr in settings.LDAP_ACL_GROUPS.iteritems():
+                        if field in results[0][1][settings.LDAP_ACL_ATTR]:
+                            try:
+                                setattr(user_profile, attr, True)
+                            except Exception as error:
+                                logger.error(error, extra = log_extra_data())
+                                raise OkupyException('LDAP ACL Groups Map is invalid')
             except AttributeError:
                 pass
-
+            '''
+            Save data in DB
+            '''
+            try:
+                user.save()
+                user_profile.user = user
+                user_profile.save()
+            except Exception as error:
+                logger.error(error, extra = log_extra_data())
+                raise OkupyException('Could not save to DB')
         return user
