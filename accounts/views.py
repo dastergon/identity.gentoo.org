f6fb57f1983bcebffa4f3553640d9ebc8eb56617
Theo Chatzimichos <tampakrap@gentoo.org>
2011-06-30 05:41:58 +0300

    Finish account view, it is able to print current or other user's profile If the other user is not in the DB yet, it gets imported

--- accounts/views.py
+++ accounts/views.py
@@ -1,10 +1,34 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render_to_response
 from django.template import RequestContext
+from okupy.accounts.backends import LDAPBackend
+from okupy.libraries.exception import OkupyException, log_extra_data
+from okupy.libraries.ldap_wrappers import ldap_user_search
+import logging
+
+logger = logging.getLogger('okupy')
+
+def checkUsername(request, username):
+    '''
+    Check if the username given in the URL is correct
+    '''
+    if username == request.user.username or ldap_user_search(username):
+        return True
+    else:
+        raise OkupyException('Invalid URL')
 
 @login_required
 def account(request, username):
-    return render_to_response('account/account.html', {}, context_instance = RequestContext(request))
+    msg = ''
+    user = ''
+    try:
+        checkUsername(request, username)
+        user = LDAPBackend()
+        user = user.get_or_create_user(username = username, other = True)
+    except OkupyException as error:
+        msg = error.value
+        logger.error(msg, extra = log_extra_data(request))
+    return render_to_response('account/account.html', {'current_user': user, 'msg': msg}, context_instance = RequestContext(request))
 
 @login_required
 def account_edit(request, username):
