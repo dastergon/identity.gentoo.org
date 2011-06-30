0d7da2df1222f18482ea8d7544707ece56b44005
Theo Chatzimichos <tampakrap@gentoo.org>
2011-06-30 18:51:03 +0300

    In InactiveEmail table, user should not be unique, the same user could have many inactive mails

--- verification/models.py
+++ verification/models.py
@@ -2,5 +2,5 @@ from django.db import models
 
 class InactiveEmail(models.Model):
     email = models.EmailField(unique = True)
-    user = models.CharField(max_length = 100, unique = True)
+    user = models.CharField(max_length = 100)
     url = models.CharField(max_length = 30, blank = True, null = True)
