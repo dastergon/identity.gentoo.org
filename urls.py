f5ce9207589be179e9be5f6e354764f36a5189cf
Theo Chatzimichos <tampakrap@gentoo.org>
2011-06-30 21:19:30 +0300

    Small update in urls

--- urls.py
+++ urls.py
@@ -1,16 +1,16 @@
 from django.conf.urls.defaults import patterns, include, url
 from django.contrib import admin
 from okupy.login.views import mylogin, mylogout
-from okupy.signup.views import *
 
 admin.autodiscover()
 
 urlpatterns = patterns('',
+    #(r'^$', include('login.urls')), <-- doesn't work
     (r'^$', include('index.urls')),
     (r'^account/', include('accounts.urls')),
     (r'^admin/', include(admin.site.urls)),
-    (r'^login/', mylogin),
-    (r'^logout/', mylogout),
+    (r'^login/$', mylogin),
+    (r'^logout/$', mylogout),
     (r'^signup/', include('signup.urls')),
     (r'^verification/', include('verification.urls')),
 )
