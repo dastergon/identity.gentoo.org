8c5605212a5de0276ed3e13988e9fabf0a82869c
Theo Chatzimichos <tampakrap@gentoo.org>
2011-07-01 05:50:34 +0300

    Add support for Edit data in the view, which will check if the user logged in is the same user that is trying to edit data, or if he is a privileged user to change other users' data. Privileged groups are defined in settings

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,10 @@
+from django.conf import settings
 from django.contrib.auth.decorators import login_required
+#from django.forms.models import modelformset_factory
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 from okupy.accounts.backends import LDAPBackend
+from okupy.accounts.models import *
 from okupy.libraries.exception import OkupyException, log_extra_data
 from okupy.libraries.ldap_wrappers import ldap_user_search
 import logging
@@ -15,21 +18,83 @@ def checkUsername(request, username):
     if username == request.user.username or ldap_user_search(username):
         return True
     else:
-        raise OkupyException('Invalid URL')
+        return False
 
 @login_required
 def account(request, username):
     msg = ''
     user = ''
     try:
-        checkUsername(request, username)
+        if not checkUsername(request, username):
+            raise OkupyException('Invalid URL')
         user = LDAPBackend()
         user = user.get_or_create_user(username = username, other = True)
     except OkupyException as error:
         msg = error.value
         logger.error(msg, extra = log_extra_data(request))
-    return render_to_response('account/account.html', {'current_user': user, 'msg': msg}, context_instance = RequestContext(request))
+    return render_to_response(
+        'account/account.html',
+        {'current_user': user, 'msg': msg},
+        context_instance = RequestContext(request))
 
 @login_required
 def account_edit(request, username):
-    return render_to_response('account/edit.html', {}, context_instance = RequestContext(request))
+    msg = ''
+    form = ''
+    try:
+        '''
+        Check if the user is a member of the privileged
+        groups that has permissions to edit other users' data
+        '''
+        for key in settings.LDAP_ACL_GROUPS_EDIT:
+            if getattr(request.user.get_profile(), key):
+                priv = True
+                break
+            else:
+                priv = False
+        if not request.user.username == username and not priv:
+            raise OkupyException('Invalid URL')
+        if not checkUsername(request, username):
+            raise OkupyException('Invalid URL')
+        '''
+        If the user is not in the DB already, he should be transfered from
+        the LDAP
+        '''
+        user = LDAPBackend()
+        user = user.get_or_create_user(username = username, other = True)
+
+        user_profile = eval(settings.AUTH_PROFILE_MODULE.split('accounts.')[1])
+        user_profile_form = eval(settings.AUTH_PROFILE_MODULE.split('accounts.')[1] + 'Form')
+        user_profile_priv_form = eval(settings.AUTH_PROFILE_MODULE.split('accounts.')[1] + 'PrivForm')
+        instance = user_profile.objects.get(user__username = username)
+        if request.method == 'POST':
+            if priv:
+                form = user_profile_priv_form(request.POST, instance = instance)
+            else:
+                form = user_profile_form(request.POST, instance = instance)
+            if form.is_valid():
+                # TODO
+                # Bind as the current user, and update the fields both in LDAP and DB
+                print 'todo'
+        else:
+            '''
+            The following code is an alternative that didn't work exactly as expected
+
+            It will stay here only for one commit, so I can have it in my git history
+            exclude = ['user', 'all_mails', 'gpg_key', 'gpg_fingerprint', 'ssh_public_key']
+            for key in settings.LDAP_ACL_GROUPS.keys():
+                exclude.append(key)
+            GentooProfileFormSet = modelformset_factory(GentooProfile, exclude = tuple(exclude))
+            form = GentooProfileFormSet(queryset = GentooProfile.objects.filter(user__username = username))
+            '''
+            if priv:
+                form = user_profile_priv_form(instance = instance)
+            else:
+                form = user_profile_form(instance = instance)
+    except OkupyException as error:
+        msg = error.value
+        logger.error(msg, extra = log_extra_data(request))
+    return render_to_response(
+        'account/edit.html',
+        {'form': form, 'msg': msg},
+        context_instance = RequestContext(request))
