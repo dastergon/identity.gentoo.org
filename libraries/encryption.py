579967450e5b1bfda30174dfad4a4e6d4c15bb2c
Theo Chatzimichos <tampakrap@gentoo.org>
2011-07-21 04:19:58 +0300

    Improve encryption functions: Add new function for checking if a string matches the ldap password Slight rename in the other functions

--- libraries/encryption.py
+++ libraries/encryption.py
@@ -2,24 +2,33 @@ from Crypto.Cipher import Blowfish
 from django.conf import settings
 from random import choice
 from base64 import encodestring as encode
+from base64 import decodestring as decode
 import base64
 import hashlib
 import string
 import os
 
-def sha1Password(password):
+def sha_password(password):
     salt = os.urandom(4)
     h = hashlib.sha1(password)
     h.update(salt)
-    return "{SSHA}" + encode(h.digest() + salt)
+    return "{SSHA}" + encode(h.digest() + salt)[:-1]
 
-def encryptPassword(password):
-    obj = Blowfish.new(settings.BLOWFISH_KEY)
-    return base64.b64encode(obj.encrypt(password))
+def check_password(challenge_password, password,):
+    challenge_bytes = decode(challenge_password[6:])
+    digest = challenge_bytes[:20]
+    salt = challenge_bytes[20:]
+    hr = hashlib.sha1(password)
+    hr.update(salt)
+    return digest == hr.digest()
 
-def decryptPassword(password):
+def encrypt_password(password):
     obj = Blowfish.new(settings.BLOWFISH_KEY)
-    original_password = obj.decrypt(base64.b64decode(password))
+    return base64.b64encode(obj.encrypt(password + settings.SECRET_KEY[:8]))
+
+def decrypt_password(password):
+    obj = Blowfish.new(settings.BLOWFISH_KEY)
+    original_password = obj.decrypt(base64.b64decode(password + settings.SECRET_KEY[:8]))
     return original_password
 
 def random_string(length, type = None):
