18b509e2e771264897fdf94fbd838dd67d772ff7
Theo Chatzimichos <tampakrap@gentoo.org>
2011-08-22 21:21:35 +0300

    typo

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ from distutils.core import setup
 
 setup(name='Okupy',
 	version='9999',
-	description='todo'
-	author='tampakrap'
+	description='todo',
+	author='tampakrap',
 	author_email='tampakrap@gentoo.org',
-	url=''
-	packages=[]
+	url='',
+	packages=[],
 )
