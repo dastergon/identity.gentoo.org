991d470b1ccb850e5225324c01b40bf0d30b9ee3
Theo Chatzimichos <tampakrap@gentoo.org>
2013-05-10 23:18:46 +0200

    accounts/urls.py will be rewritten as well

--- identity/accounts/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import patterns
-
-urlpatterns = patterns('identity.accounts.views',
-    (r'^(?P<username>\w+)/$', 'account'),
-	(r'^all/$', 'account_all'),
-    (r'^(?P<username>\w+)/edit/$', 'account_edit'),
-    (r'^(?P<username>\w+)/edit/password/$', 'account_edit_password'),
-    (r'^(?P<username>\w+)/edit/email/$', 'account_edit_email'),
-)
