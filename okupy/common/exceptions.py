f87b630aa9b83f1a89a0699bd9b267587499ddbf
Theo Chatzimichos <tampakrap@gentoo.org>
2013-05-11 00:04:56 +0200

    Small fixes in OkupyError, add LoginError

--- okupy/common/exceptions.py
+++ okupy/common/exceptions.py
@@ -1,10 +1,19 @@
-from django.contrib.sites.models import Site
+# -*- coding: utf-8 -*-
 
-class OkupyException(Exception):
+class OkupyError(Exception):
     '''
-    Custon exception class
+    Custon exception class for general errors
     '''
     def __init__(self, value):
         self.value = value
     def __str__(self):
-        return repr(self.value)
+        return unicode(self.value).encode('utf-8')
+
+class LoginError(Exception):
+    '''
+    Custom exception class for login failures
+    '''
+    def __init__(self):
+        self.value = u'Login failed'
+    def __str__(self):
+        return unicode(self.value).encode('utf-8')
