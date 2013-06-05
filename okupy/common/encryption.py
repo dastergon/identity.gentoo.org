d093d6af64bcb4b96e7f336682b76e55b40ab592
Theo Chatzimichos <tampakrap@gentoo.org>
2013-06-06 01:47:07 +0200

    Revert AES to Blowfish and my previous implementation
    The new one didn't work as expected
    Remove sha1_password and the verify function, as they will not be
    used. Instead, we will use passlib.hash.ldap_md5_crypt

--- okupy/common/encryption.py
+++ okupy/common/encryption.py
@@ -1,4 +1,4 @@
-from Crypto.Cipher import AES
+from Crypto.Cipher import Blowfish
 from django.conf import settings
 from random import choice
 import base64
@@ -6,49 +6,22 @@ import hashlib
 import os
 import string
 
-def sha1_password(password):
-    '''
-    Create a SHA1 salted hash
-    '''
-    salt = os.urandom(4)
-    h = hashlib.sha1(password)
-    h.update(salt)
-    return "{SSHA}" + base64.encodestring(h.digest() + salt)[:-1]
-
-def check_password(challenge_password, password,):
-    challenge_bytes = decode(challenge_password[6:])
-    digest = challenge_bytes[:20]
-    salt = challenge_bytes[20:]
-    hr = hashlib.sha1(password)
-    hr.update(salt)
-    return digest == hr.digest()
-
 def encrypt_password(password):
     '''
-    Encrypt the password in AES encryption, using the secret key
+    Encrypt the password in Blowfish encryption, using the secret key
     specified in the settings file
-    Taken from
-    http://www.codekoala.com/blog/2009/aes-encryption-python-using-pycrypto/
     '''
-    BLOCK_SIZE = 32
-    PADDING = '{'
-    pad = lambda s: s + (BLOCK_SIZE - len(s) % BLOCK_SIZE) * PADDING
-    EncodeAES = lambda c, s: base64.b64encode(c.encrypt(pad(s)))
-    cipher = AES.new(settings.SECRET_KEY[:BLOCK_SIZE])
-    return EncodeAES(cipher, password)
+    obj = Blowfish.new(settings.SECRET_KEY)
+    return base64.b64encode(obj.encrypt(password + settings.SECRET_KEY[:8]))
 
 def decrypt_password(password):
     '''
-    Decrypt the password in AES encryption, using the secret key
+    Decrypt the password in Blowfish encryption, using the secret key
     specified in the settings file
-    Taken from
-    http://www.codekoala.com/blog/2009/aes-encryption-python-using-pycrypto/
     '''
-    BLOCK_SIZE = 32
-    PADDING = '{'
-    DecodeAES = lambda c, e: c.decrypt(base64.b64decode(e)).rstrip(PADDING)
-    cipher = AES.new(settings.SECRET_KEY[:BLOCK_SIZE])
-    return DecodeAES(cipher, password)
+    obj = Blowfish.new(settings.SECRET_KEY)
+    origin_pass = obj.decrypt(base64.b64decode(password + settings.SECRET_KEY[:8]))
+    return origin_pass[:-8]
 
 def random_string(length):
     '''
