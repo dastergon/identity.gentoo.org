ac0006ccd6f4c00e96748a018ede6d3dd23382d0
Theo Chatzimichos <tampakrap@gentoo.org>
2013-06-09 20:18:50 +0200

    rename dictionary to avoid confusion with the function name

--- okupy/common/log.py
+++ okupy/common/log.py
@@ -10,25 +10,25 @@ def log_extra_data(additional = None):
     * If the additional argument is the request, then from the request it
     prints the client IP and username if applicable.
     '''
-    log_extra_data = {
+    extra_data = {
         'client_ip': '',
         'id_name': '',
         'instance_name': settings.INSTANCE_NAME,
     }
     if not additional:
-        return log_extra_data
+        return extra_data
     if type(additional) == str or type(additional) == unicode:
-        log_extra_data['id_name'] = additional
+        extra_data['id_name'] = additional
     else:
         request = additional
         if request.META:
-            log_extra_data['client_ip'] = request.META.get('REMOTE_ADDR', 'None')
+            extra_data['client_ip'] = request.META.get('REMOTE_ADDR', 'None')
         try:
             if request.user.is_authenticated():
                 '''
                 Handle logged in users
                 '''
-                log_extra_data['id_name'] = request.user.name
+                extra_data['id_name'] = request.user.name
         except AttributeError:
                 pass
-    return log_extra_data
+    return extra_data
