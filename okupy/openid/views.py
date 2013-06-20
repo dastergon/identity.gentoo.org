d977af05d3f0633f992d65c9506439400cf16319
Michał Górny <mgorny@gentoo.org>
2013-06-20 12:58:50 +0200

    [OpenID] Enable Return-To trust checks.
    Just display the results for now.

--- okupy/openid/views.py
+++ okupy/openid/views.py
@@ -17,6 +17,9 @@ from openid.store.filestore import FileOpenIDStore
 from openid.server.server import Server, ProtocolError, EncodingError, \
         CheckIDRequest
 
+# for exceptions
+import openid.yadis.discover, openid.fetchers
+
 from .openid_store import DjangoDBOpenIDStore
 
 def login(request):
@@ -130,7 +133,22 @@ def auth_site(request):
         del request.session['openid_request']
         return render_openid_response(request, oresp)
 
+    if not oreq.trustRootValid():
+        tr_valid = 'Return-To invalid (not under trust root)'
+    else:
+        try:
+            # XXX: cache it
+            if oreq.returnToVerified():
+                tr_valid = 'Return-To valid and trusted'
+            else:
+                tr_valid = 'Return-To untrusted'
+        except openid.yadis.discover.DiscoveryFailure:
+            tr_valid = 'Unable to verify trust (Yadis unsupported)'
+        except openid.fetchers.HTTPFetchingError:
+            tr_valid = 'Unable to verify trust (HTTP error)'
+
     return render(request, 'openid/auth-site.html',
             {
-                'request': oreq
+                'request': oreq,
+                'return_to_valid': tr_valid,
             })
