f5b42ccc45e63a187e1a8bc547094e07f26f7aa1
Michał Górny <mgorny@gentoo.org>
2013-06-26 16:27:14 +0200

    Use url() in urlpatterns.

--- okupy/accounts/urls.py
+++ okupy/accounts/urls.py
@@ -4,10 +4,10 @@ from django.conf.urls import patterns, url
 from .views import login, index, signup, activate, formerdevlist, foundationlist
 
 accounts_urlpatterns = patterns('',
-    (r'^$', index),
-    (r'^login/$', login),
-    (r'^former-devlist/$', formerdevlist),
-    (r'^foundation-members/$', foundationlist),
-    (r'^signup/$', signup),
-    (r'^activate/(?P<token>[a-zA-Z0-9]+)/$', activate),
+    url(r'^$', index),
+    url(r'^login/$', login),
+    url(r'^former-devlist/$', formerdevlist),
+    url(r'^foundation-members/$', foundationlist),
+    url(r'^signup/$', signup),
+    url(r'^activate/(?P<token>[a-zA-Z0-9]+)/$', activate),
 )
