73695eb356f99929a5d351209fae9de2fd9e8303
Michał Górny <mgorny@gentoo.org>
2013-06-29 19:08:59 +0200

    Drop separate OpenID after merging it.

--- okupy/urls.py
+++ okupy/urls.py
@@ -1,8 +1,7 @@
 # vim:fileencoding=utf8:et:ts=4:sts=4:sw=4:ft=python
-from django.conf.urls import patterns, include, url
+from django.conf.urls import patterns, include
 
 from .accounts.urls import accounts_urlpatterns
-from .openid.urls import openid_urlpatterns
 
 urlpatterns = patterns('',
     (r'^', include(accounts_urlpatterns)),
