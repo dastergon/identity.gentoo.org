4db6789d284a06226edbfb11e41a1ba70a8bd979
Michał Górny <mgorny@gentoo.org>
2013-08-08 20:40:40 +0200

    urls: import the whole module rather than single views.
    We import all the views anyway, so we end up repeating every name twice,
    and potentially polluting the namespace.

--- okupy/accounts/urls.py
+++ okupy/accounts/urls.py
@@ -1,19 +1,19 @@
 # vim:fileencoding=utf8:et:ts=4:sts=4:sw=4:ft=python
 from django.conf.urls import patterns, url
-from .views import (DevListsView, ssl_auth, login, logout, index, signup,
-        activate, openid_endpoint, user_page, openid_auth_site)
+
+from . import views as v
 
 accounts_urlpatterns = patterns('',
-    url(r'^$', index),
-    url(r'^login/$', login),
-    url(r'^ssl-auth/$', ssl_auth),
-    url(r'^logout/$', logout),
-    url(r'^devlist/$', DevListsView.as_view(template_name='devlist.html')),
-    url(r'^former-devlist/$', DevListsView.as_view(template_name='former-devlist.html')),
-    url(r'^foundation-members/$', DevListsView.as_view(template_name='foundation-members.html')),
-    url(r'^signup/$', signup),
-    url(r'^activate/(?P<token>[a-zA-Z0-9]+)/$', activate),
-    url(r'^endpoint/$', openid_endpoint),
-    url(r'^id/(.*)/$', user_page),
-    url(r'^auth-site/$', openid_auth_site),
+    url(r'^$', v.index),
+    url(r'^login/$', v.login),
+    url(r'^ssl-auth/$', v.ssl_auth),
+    url(r'^logout/$', v.logout),
+    url(r'^devlist/$', v.DevListsView.as_view(template_name='devlist.html')),
+    url(r'^former-devlist/$', v.DevListsView.as_view(template_name='former-devlist.html')),
+    url(r'^foundation-members/$', v.DevListsView.as_view(template_name='foundation-members.html')),
+    url(r'^signup/$', v.signup),
+    url(r'^activate/(?P<token>[a-zA-Z0-9]+)/$', v.activate),
+    url(r'^endpoint/$', v.openid_endpoint),
+    url(r'^id/(.*)/$', v.user_page),
+    url(r'^auth-site/$', v.openid_auth_site),
 )
