dc0addfff53bb2551cb394df9a541cd61f55b246
Theo Chatzimichos <tampakrap@gentoo.org>
2013-08-11 00:33:25 +0200

    Convert a bunch of integration to unit tests

--- okupy/tests/integration/login.py
+++ okupy/tests/integration/login.py
@@ -2,14 +2,11 @@
 
 from django.conf import settings
 from django.contrib.auth.models import User
-from django.core import mail
-from django.db import DatabaseError
 from django.test.client import Client
+
 from mockldap import MockLdap
 
 from ...common.test_helpers import OkupyTestCase
-
-import mock
 
 
 account1 = {'username': 'alice', 'password': 'ldaptest'}
@@ -18,9 +15,6 @@ wrong_account = {'username': 'wrong', 'password': 'wrong'}
 
 
 class LoginTestsEmptyDB(OkupyTestCase):
-    cursor_wrapper = mock.Mock()
-    cursor_wrapper.side_effect = DatabaseError
-
     @classmethod
     def setUpClass(cls):
         cls.mockldap = MockLdap(settings.DIRECTORY)
@@ -37,19 +31,6 @@ class LoginTestsEmptyDB(OkupyTestCase):
         response = self.client.get('/login/')
         self.assertTemplateUsed(response, 'base.html')
         self.assertTemplateUsed(response, 'login.html')
-
-    def test_empty_user(self):
-        response = self.client.post('/login/')
-        self.assertFormError(response, 'login_form', 'username', [u'This field is required.'])
-        self.assertFormError(response, 'login_form', 'password', [u'This field is required.'])
-        self.assertMessage(response, 'Login failed', 40)
-        self.assertEqual(User.objects.count(), 0)
-
-    def test_incorrect_user(self):
-        wrong_account = {'username': 'username', 'password': 'password'}
-        response = self.client.post('/login/', wrong_account)
-        self.assertMessage(response, 'Login failed', 40)
-        self.assertEqual(User.objects.count(), 0)
 
     def test_correct_user_post_login_redirect(self):
         account = account1.copy()
@@ -69,20 +50,6 @@ class LoginTestsEmptyDB(OkupyTestCase):
         self.assertEqual(user.last_name, '')
         self.assertEqual(user.email, '')
 
-    def test_no_ldap(self):
-        self.mockldap.stop()
-        response = self.client.post('/login/', account1)
-        self.assertMessage(response, 'Login failed', 40)
-        self.assertEqual(User.objects.count(), 0)
-        self.mockldap.start()
-
-    @mock.patch("django.db.backends.util.CursorWrapper", cursor_wrapper)
-    def test_no_database(self):
-        response = self.client.post('/login/', account1)
-        self.assertMessage(response, "Can't contact the LDAP server or the database", 40)
-        self.assertEqual(len(mail.outbox), 1)
-        self.assertTrue(mail.outbox[0].subject.startswith('%sERROR:' % settings.EMAIL_SUBJECT_PREFIX))
-
     def test_already_authenticated_user_redirects_to_index(self):
         response = self.client.post('/login/', account1)
         response = self.client.get('/login/')
@@ -96,6 +63,7 @@ class LoginTestsEmptyDB(OkupyTestCase):
     def test_logout_for_anonymous_user_redirects_to_login(self):
         response = self.client.get('/logout/')
         self.assertRedirects(response, '/login/')
+
 
 class LoginTestsOneAccountInDB(OkupyTestCase):
     fixtures = ['alice']
@@ -111,13 +79,6 @@ class LoginTestsOneAccountInDB(OkupyTestCase):
 
     def tearDown(self):
         self.mockldap.stop()
-
-    def test_dont_authenticate_from_db_when_ldap_is_down(self):
-        self.mockldap.stop()
-        response = self.client.post('/login/', account1)
-        self.assertMessage(response, 'Login failed', 40)
-        self.assertEqual(User.objects.count(), 1)
-        self.mockldap.start()
 
     def test_authenticate_account_that_is_already_in_db(self):
         response = self.client.post('/login/', account1)
--- okupy/tests/unit/views.py
+++ okupy/tests/unit/views.py
@@ -1,10 +1,18 @@
 # vim:fileencoding=utf8:et:ts=4:sts=4:sw=4:ft=python
 
+from django.conf import settings
+from django.contrib.auth.models import User
+from django.core import mail
 from django.core.urlresolvers import resolve
+from django.db import DatabaseError
+from django.template import RequestContext
 from django.test import TestCase
 
 from ...accounts.views import login, index, signup
+from ...accounts.forms import LoginForm
 from ...common.test_helpers import OkupyTestCase, set_request
+
+import mock
 
 
 account1 = {'username': 'alice', 'password': 'ldaptest'}
@@ -12,7 +20,7 @@ account2 = {'username': 'bob', 'password': 'ldapmoretest'}
 wrong_account = {'username': 'wrong', 'password': 'wrong'}
 
 
-class LoginViewTests(TestCase):
+class LoginViewTests(OkupyTestCase):
     def test_login_url_resolves_to_login_view(self):
         found = resolve('/login/')
         self.assertEqual(found.func, login)
@@ -21,6 +29,69 @@ class LoginViewTests(TestCase):
         request = set_request(uri='/login')
         response = login(request)
         self.assertEqual(response.status_code, 200)
+
+    def test_rendered_login_form(self):
+        request = set_request(uri='/login')
+        response = login(request)
+        login_form_part = '<input id="id_username" maxlength="100" name="username" type="text" />'
+        self.assertIn(login_form_part, response.content)
+
+    def test_empty_user_raises_form_error_messages(self):
+        request = set_request(uri='/login')
+        response = login(request)
+        response.context = RequestContext(request, {'login_form': LoginForm(request.POST)})
+        self.assertFormError(response, 'login_form', 'username', 'This field is required.')
+        self.assertFormError(response, 'login_form', 'password', 'This field is required.')
+
+    def test_empty_user_raises_login_failed(self):
+        request = set_request(uri='/login', post=True, messages=True)
+        response = login(request)
+        response.context = RequestContext(request)
+        self.assertMessage(response, 'Login failed', 40)
+
+    def test_dont_authenticate_from_db_when_ldap_is_down(self):
+        request = set_request(uri='/login', post=account2, messages=True)
+        response = login(request)
+        response.context = RequestContext(request)
+        self.assertMessage(response, 'Login failed', 40)
+
+    def test_incorrect_user_raises_login_failed(self):
+        request = set_request(uri='/login', post=wrong_account, messages=True)
+        response = login(request)
+        response.context = RequestContext(request)
+        self.assertMessage(response, 'Login failed', 40)
+
+    def test_incorrect_user_does_not_get_transferred_in_db(self):
+        request = set_request(uri='/login', post=wrong_account, messages=True)
+        response = login(request)
+        response.context = RequestContext(request)
+        self.assertEqual(User.objects.count(), 0)
+
+    def test_no_ldap(self):
+        request = set_request(uri='/login', post=wrong_account, messages=True)
+        response = login(request)
+        response.context = RequestContext(request)
+        self.assertMessage(response, 'Login failed', 40)
+
+
+class LoginViewTestsNoDatabase(OkupyTestCase):
+    cursor_wrapper = mock.Mock()
+    cursor_wrapper.side_effect = DatabaseError
+
+    @mock.patch("django.db.backends.util.CursorWrapper", cursor_wrapper)
+    def test_no_database_raises_critical(self):
+        request = set_request(uri='/login', post=account1, messages=True)
+        response = login(request)
+        response.context = RequestContext(request)
+        self.assertMessage(response, "Can't contact the LDAP server or the database", 40)
+
+    @mock.patch("django.db.backends.util.CursorWrapper", cursor_wrapper)
+    def test_no_database_sends_notification_mail(self):
+        request = set_request(uri='/login', post=account1, messages=True)
+        response = login(request)
+        response.context = RequestContext(request)
+        self.assertEqual(len(mail.outbox), 1)
+        self.assertTrue(mail.outbox[0].subject.startswith('%sERROR:' % settings.EMAIL_SUBJECT_PREFIX))
 
 
 class IndexViewTests(TestCase):
