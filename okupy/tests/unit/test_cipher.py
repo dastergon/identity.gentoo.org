87947a5b36e6d347e5bf89129fba23cfeb53f1b3
Michał Górny <mgorny@gentoo.org>
2013-08-20 21:14:39 +0200

    test_cipher: respect block_size.

--- okupy/tests/unit/test_cipher.py
+++ okupy/tests/unit/test_cipher.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+# vim:fileencoding=utf8:et:ts=4:sts=4:sw=4:ft=python
 
 from Crypto import Random
 from unittest import TestCase
@@ -7,23 +7,30 @@ from ...common.crypto import cipher
 
 
 class OkupyCipherTests(TestCase):
-    def test_verify_password_less_than_8_chars(self):
-        hash = cipher.encrypt('test1')
-        self.assertEqual(cipher.decrypt(hash, 5), 'test1')
+    def setUp(self):
+        self._random_string = '123456abcdef' * int(cipher.block_size / 2)
 
-    def test_verify_password_8_chars(self):
-        hash = cipher.encrypt('testtest')
-        self.assertEqual(cipher.decrypt(hash, 8), 'testtest')
+    def test_verify_password_less_than_block_size(self):
+        data = self._random_string[:cipher.block_size-3]
+        hash = cipher.encrypt(data)
+        self.assertEqual(cipher.decrypt(hash, len(data)), data)
 
-    def test_verify_password_more_than_8_chars(self):
-        hash = cipher.encrypt('testtest123')
-        self.assertEqual(cipher.decrypt(hash, 11), 'testtest123')
+    def test_verify_password_exact_block_size(self):
+        data = self._random_string[:cipher.block_size]
+        hash = cipher.encrypt(data)
+        self.assertEqual(cipher.decrypt(hash, len(data)), data)
 
-    def test_verify_password_more_than_16_chars(self):
-        hash = cipher.encrypt('testtest123456789012')
-        self.assertEqual(cipher.decrypt(hash, 20), 'testtest123456789012')
+    def test_verify_password_more_than_block_size(self):
+        data = self._random_string[:cipher.block_size+3]
+        hash = cipher.encrypt(data)
+        self.assertEqual(cipher.decrypt(hash, len(data)), data)
+
+    def test_verify_password_more_than_twice_block_size(self):
+        data = self._random_string[:cipher.block_size*2+3]
+        hash = cipher.encrypt(data)
+        self.assertEqual(cipher.decrypt(hash, len(data)), data)
 
     def test_encrypt_random_bytes(self):
-        password = Random.get_random_bytes(45)
-        hash = cipher.encrypt(password)
-        self.assertEqual(cipher.decrypt(hash, 45), password)
+        data = Random.get_random_bytes(45)
+        hash = cipher.encrypt(data)
+        self.assertEqual(cipher.decrypt(hash, len(data)), data)
